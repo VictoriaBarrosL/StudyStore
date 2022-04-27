5dcf2d93c51738543141a4da15885ac0cc0a3171
Victoria Barros <lis.victoriab@gmail.com>
2022-04-27 18:48:59 -0300

    Project Created

---
+++ src/StudyStore.Data/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace StudyStore.Data
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ src/StudyStoreApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StudyStoreApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
